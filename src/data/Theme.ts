17bed8813cde17165eaad73087ac3fc870322c3a
Arturo Riter <arturo.riter@gmail.com>
2020-03-08 17:51:12 +0100

    main workflow
    build and deploy

---
+++ src/data/Theme.ts
@@ -0,0 +1,25 @@
+export interface Theme {
+    page: {
+      textColor: string;
+      backgroundColor: string;
+    };
+    h1: {
+      backgroundColor: string;
+      textColor: string;
+    };
+    header: {
+      backgroundColor: string;
+      textColor: string;
+    };
+    footer: {
+      backgroundColor: string;
+      textColor: string;
+    };
+    button: {
+      backgroundColor: string;
+      textColor: string;
+    };
+    linkedinBadge: string;
+    table: string;
+  }
+  
